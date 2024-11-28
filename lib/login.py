49edb928722dd5e094f7c26ff31522fdf6df8f0c
TeaH4nd <alexchamon@poli.ufrj.br>
2024-11-28 19:31:13 -0300

    formatter 2

--- lib/login.py
+++ lib/login.py
@@ -11,10 +11,14 @@ def get_user_dir(user_id):
     return os.path.join(BASE_DIR, str(user_id))
 
 # Função para obter o caminho do arquivo de dados do usuário
+
+
 def get_user_data_file(user_id):
     return os.path.join(get_user_dir(user_id), "dados.json")
 
 # Função para salvar os dados do usuário
+
+
 def save_user_data(user_id, dados):
     user_dir = get_user_dir(user_id)
     os.makedirs(user_dir, exist_ok=True)
@@ -22,6 +26,8 @@ def save_user_data(user_id, dados):
         json.dump(dados, file)
 
 # Função para carregar os dados do usuário
+
+
 def load_user_data(user_id):
     try:
         with open(get_user_data_file(user_id), "r") as file:
